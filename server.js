f5452fa963833257abd07c062f2d3e9b484af968
Emma Dickson <emmadickson@Emmas-MacBook-Air.local>
2024-11-08 11:54:27 -0500

    deploy?

--- server.js
+++ server.js
@@ -12,7 +12,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-
+app.listen(process.env.PORT || 3000);
